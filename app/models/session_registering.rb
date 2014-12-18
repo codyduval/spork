9ae1591e22faf544a5d995f2891896b86c1a2ec1
Cody Duval <cody.duval@gmail.com>
2014-12-18 17:15:50 -0500

    moved all into a single context

--- app/models/session_registering.rb
+++ app/models/session_registering.rb
@@ -4,39 +4,79 @@
 # Preconditions: parent is already authenticated and registered
 
 class SessionRegistering
-   def initialize(parent_user_id, child_user_id, play_session_id)
-     @child = find_child(child_user_id)
-     @user = find_parent(parent_user_id)
-     @play_session = find_play_session(play_session_id)
-   end
 
-   #Trigger: parent selects play session to register for 
-   def execute
-     @parent.assign_child_to_session(@child, @play_session)
-     #2. system assigns child to session
-     #3. parent submits payment
-     #4. parent receives confirmation via email
-     @play_session
-   end
+  def self.start(current_user_id)
+    SessionRegistering.new.start(current_user_id)
+  end
 
-  module Parent
-    def assign_child_to_session(child, play_session)
-      child.play_sessions << play_session
-      child.save
+  def self.browse(semester_id)
+    SessionRegistering.new.browse(semester_id)
+  end
+
+  def self.register(child_id, play_session_id)
+    SessionRegistering.new.register(child_id, play_session_id)
+  end
+
+  def start(current_user_id)
+    parent = find_person(current_user_id).extend(Parent)
+    children = parent.children
+    semesters = open_semesters
+    {:children => children, :semesters => semesters}
+  end
+
+  def browse(semester_id)
+    semester = find_semester(semester_id).extend(SelectedSemester)
+    open_sessions = semester.open_sessions
+    open_sessions
+  end
+
+  def register(child_id, play_session_id)
+    child = find_child(child_user_id)
+    play_session = find_play_session(play_session_id).extend(OpenPlaySession)
+    play_session.add(child)
+    play_session.save
+  end
+
+  def finish(registration_id)
+    confirm = find_registration(registration_id)
+  end
+
+  module SelectedSemester
+    def open_sessions
+      self.play_sessions
     end
   end
 
-  def find_child(child_id)
-    child = User.find_by_id(child_id)
+  module Parent
+    def children
+      self.children
+    end
   end
 
-  def find_parent(parent_id)
-    parent = User.find_by_id(parent_id)
-    parent = parent.extend(Parent)
+  module OpenPlaySession
+    def add(child)
+      self.children << child
+    end
+  end
+
+  def find_semester(semester_id)
+    Semester.find_by_id(semester_id)
+  end
+
+  def find_person(user_id)
+    User.find_by_id(child_id)
   end
 
   def find_play_session(play_session_id)
-    play_session = PlaySession.find_by_id(play_session_id)
+    PlaySession.find_by_id(play_session_id)
+  end
+
+  def open_semesters
+    Semester.where(:open_for_reg => true)
+  end
+
+  def find_registration
+    Registration.find_by_id(registration_id)
   end
 
 end
